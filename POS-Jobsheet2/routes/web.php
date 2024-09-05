1541735ddcf3f591d83b25b3f6f9236ce65bacd4
= <=>
2024-09-05 19:51:06 +0700

    Jobsheet 2 - POS

--- POS-Jobsheet2/routes/web.php
+++ POS-Jobsheet2/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SaleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('food-beverage', [ProductController::class, 'index'])->nama('products.food-beverage');
+    Route::get('beauty-health', [ProductController::class, 'index'])->nama('products.beauty-health');
+    Route::get('home-care', [ProductController::class, 'index'])->nama('products.home-care');
+    Route::get('baby-kid', [ProductController::class, 'index'])->nama('products.baby-kid');
 });
+
+Route::get('/user/{id}/nama/{nama}', [UserController::class, 'show']);
+
+Route::get('/sales', [SaleController::class, 'index']);
