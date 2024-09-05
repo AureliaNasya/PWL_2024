f2e3f9ed62b38e61bd4c2eb2c39e1561aaf1f5e1
= <=>
2024-09-05 20:07:44 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function index()
+    public function __invoke()
     {
         return view('home');
     }
