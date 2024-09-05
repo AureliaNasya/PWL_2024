18dbabdc40e361fa38575e5ff3e9400badf302bf
= <=>
2024-09-05 17:40:35 +0700

    Jobsheet 2 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\TextUI\Configuration\Php;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,3 +50,12 @@ Route::get('/user/{name?}', function ($name='John') {
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
 Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+    ]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+    ]);
