5e891e93143c090a348f3735ab290d54771283e7
= <=>
2024-09-18 09:01:46 +0700

    Jobsheet 4 - Praktikum 2.1

--- Jobsheet2_POS/app/Http/Controllers/UserController.php
+++ Jobsheet2_POS/app/Http/Controllers/UserController.php
@@ -8,7 +8,20 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index() {   
+        $user = UserModel::findOr(20,['username', 'nama'], function() {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+        /*
+        $user = UserModel::firstWhere('level_id', 1);
+        return view('user', ['data' => $user]);
+        
+        $user = UserModel::where('level_id', 1)->first();
+        return view('user', ['data' => $user]);
+
+        $user = UserModel::find(1);
+        return view('user', ['data' => $user]);
         $data = [
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
@@ -17,9 +30,8 @@ class UserController extends Controller
         ];
         UserModel::insert($data);
         $user = UserModel::all();
-        return view('user', ['data' => $user]);
 
-        /*
+        return view('user', ['data' => $user]);
         $data = [
             'nama' => 'Pelanggan Pertama',
         ];
