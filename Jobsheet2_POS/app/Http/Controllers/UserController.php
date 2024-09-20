a35dd382b8779a0ded0600f156ad9cba49ece06a
= <=>
2024-09-20 10:39:32 +0700

    Jobsheet 4 - Praktikum 2.4

--- Jobsheet2_POS/app/Http/Controllers/UserController.php
+++ Jobsheet2_POS/app/Http/Controllers/UserController.php
@@ -9,8 +9,40 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {   
-        $user = UserModel::where('level_id', 2)->count();
-        /*dd($user);*/
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manajer Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+        
         return view('user', ['data' => $user]);
+        /*
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager',
+                'nama' => 'Manager',
+            ],
+        );
+        
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager22',
+                'nama' => 'Manager Dua Dua',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        ); 
+
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager',
+                'nama' => 'Manager',
+            ],
+        );
+        */
     }
 }
