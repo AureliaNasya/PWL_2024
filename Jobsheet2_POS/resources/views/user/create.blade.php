8e9680fc963100fc5ca1ab3c5b5a64a02d6466d7
= <=>
2024-10-05 22:56:03 +0700

    Jobsheet 5 - Praktikum 3

--- Jobsheet2_POS/resources/views/user/create.blade.php
+++ Jobsheet2_POS/resources/views/user/create.blade.php
@@ -33,6 +33,15 @@
                 </div>
             </div>
             <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Nama</label>
+                <div class="col-11">
+                    <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" required>
+                    @error('nama')
+                        <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group row">
                 <label class="col-1 control-label col-form-label">Password</label>
                 <div class="col-11">
                     <input type="password" class="form-control" id="password" name="password" required>
