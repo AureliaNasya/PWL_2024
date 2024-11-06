c875c99205758aa46024357c508153d0d2c0a015
= <=>
2024-11-06 19:37:51 +0700

    Jobsheet 10 - Praktikum 4

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\Api\RegisterController;
 use App\Controllers\Api\LoginController;
+use App\Http\Controllers\API\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\API\LogoutController::class)->name('logout');
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
