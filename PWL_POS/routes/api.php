557ddd9dcf14afbb6e48be6d1786a3eaa3523e30
= <=>
2024-11-06 17:48:23 +0700

    Jobsheet 10 - Praktikum 2

--- PWL_POS/routes/api.php
+++ PWL_POS/routes/api.php
@@ -1,18 +1,12 @@
 <?php
 
 use App\Controllers\Api\RegisterController;
+use App\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::post('/register', App\Http\Controllers\API\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\API\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
